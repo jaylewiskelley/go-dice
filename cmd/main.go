1dc909da470dcf1ea7131389f8c5bf2f80c5f11f
Jay Lewis Kelley <jay@jaykelley.dev>
2021-03-18 01:10:27 +0000

    feat: add 1-6 rand num

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"math/rand"
 )
 
 func main() {
 	fmt.Println("Welcome to GO DICE!")
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println(rand.Intn(6) + 1)
 }
